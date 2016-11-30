f6b2378b1ffaea3876acd5a1746efcf91a5d682e
Peter Nelson <pcnelson@calpoly.edu>
2016-11-30 13:52:15 -0700

    some fiddling

---
+++ PHYS202/ConfigureGit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+git config --global user.name "Peter Nelson"
+git config --global user.email "pcnelson@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
