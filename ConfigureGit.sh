f42647fa1503f21c973c40c693210b018d5f3bd8
Peter Nelson <pcnelson@calpoly.edu>
2016-11-30 14:12:41 -0700

    some fiddling

--- ConfigureGit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-git config --global user.name "Peter Nelson"
-git config --global user.email "pcnelson@calpoly.edu"
-git config --global color.ui "auto"
-git config --global core.editor "nano"
--- iPython/mymodule.py
+++
@@ -1,9 +0,0 @@
-"""A simple demonstration module"""
-
-def add_numbers(x,y):
-    """add x and y"""
-    return x + y
-
-def subtract_numbers(x,y):
-    """subtract y from x"""
-    return x - y
